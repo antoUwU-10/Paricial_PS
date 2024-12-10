877f99d90d1703f2a218dc7b7a2500228be9d78e
antoUwU-10 <anto.uwu1008@gmail.com>
2024-12-10 10:32:09 -0400

    Hola modificado desde Flow

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo
+          Hola Mundo desde Flow
         </p>
         <a
           className="App-link"
